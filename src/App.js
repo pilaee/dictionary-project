a9495a89511ae1d62ca9d5947d4ed6fd3a00fd9a
pollina iliev <pilaeev@hotmail.com>
2021-06-19 18:33:18 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+    <div className="container">
       <header className="App-header">
-      SheCodes
-      <a href="#"className=" btn btn-primary">
-      Hello
-      </a>
+      Find your word 
       </header>
+      <main>
+      <Dictionary />
+      </main>
+      <footer className="App-footer">
+      <small> Coded by Pollina Iliev</small>
+      </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
